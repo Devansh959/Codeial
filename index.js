ef45c2adf34c55d2344251230d5b95fea9c54d43
Swant Arya <swantarya@Swants-MacBook-Air.local>
2022-09-15 14:19:33 +0530

    First Commit: Setting up directory structure and express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express= require('express');
+const port= 3000;
+const app= express();
+
+
+app.listen(port, function(err){
+    if(err){
+        console.log("Error in loading the Server")
+    }
+    console.log(`Server is running on port: ${port} `);
+})
