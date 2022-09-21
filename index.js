c8ab0f48caa719a04e1de87ed3b184740d7ecb2f
Swant Arya <swantarya@Swants-MacBook-Air.local>
2022-09-21 17:08:34 +0530

    setup static file access for layouts and pages

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express= require('express');
 const port= 3000;
 const app= express();
+const expressLayouts= require("express-ejs-layouts");
+app.use(express.static("./assets"));
+app.use(expressLayouts);
 app.use('/', require("./routes"))
+app.set("layout extractStyles",true);
+app.set("layout extractScripts",true);
+
+app.set("view engine","ejs");
+app.set('views','./views');
+
 
 app.listen(port, function(err){
     if(err){
---
+++ routes/users.js
@@ -0,0 +1,6 @@
+const express= require("express");
+const router= express.Router();
+const userContoller= require("../controllers/users_controller");
+
+router.get('/profile', userContoller.profile );
+module.exports= router;
