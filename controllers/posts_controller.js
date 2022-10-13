4143522b4e56b8e0daa470de2073a5ba2e72fc30
Swant Arya <swantarya@Swants-MacBook-Air.local>
2022-10-13 20:54:42 +0530

    flash notificatiion added

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,41 +1,41 @@
 const Post= require('../models/post');
 const Comment= require('../models/comment');
 
-module.exports.create = function(req,res){
+module.exports.create = async function(req,res){
     
-    
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    },function(err,post){
-        
-        if(err){
-            console.log('error in creating a post'); return;
-        }
-        
+    try{
+        await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        });
+        req.flash('success','Post Published')
         return res.redirect('back');
-    })
-}
+    }catch(err){
+        req.flash('error',err)
+        return res.redirect('back');
 
-module.exports.destroy = function(req,res){
-    Post.findById(req.params.id , function(err,post){
-        if(err){
-            console.log("Couldn't find Post ");
-            return;
-        }
+    }
+};
+  
+
+module.exports.destroy = async function(req,res){
+    try{
+        let post= await Post.findById(req.params.id);
         //.id means converting objectId in string 
         if(post.user==req.user.id){
             post.remove();
-            Comment.deleteMany({post:req.params.id}, function(err){
-                return res.redirect('back');
-
-            });
+            await Comment.deleteMany({post:req.params.id});
+            req.flash('success','Post Deleted')
+            return res.redirect('back');
         }else{
+            req.flash('error','You Cannot Delete this Post')
             return res.redirect('back');
         }
 
-
-
-
-    })
-}
+    }catch(err){
+        req.flash('error',err)
+        
+        return;
+    }
+    
+    }
--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const passport= require('passport');
 const passportLocal= require("./config/passport-local-strategy");
 const MongoStore = require('connect-mongo');
 const sassMiddleware= require("node-sass-middleware");
+const flash= require('connect-flash');
+const customMware= require('./config/middleware');
 app.use(sassMiddleware({
     src: './assets/scss',
     dest: './assets/css',
@@ -57,6 +59,8 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
+app.use(flash());
+app.use(customMware.setFlash);
 app.use('/', require("./routes"))
 
 
